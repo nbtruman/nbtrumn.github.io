3750147c593132ee0ad7e8c8f957f800da11bdf6
nbtruman <104936442+nbtruman@users.noreply.github.com>
2022-06-23 22:42:25 +0100

    add comments on JS

--- JS/MenuSlider.js
+++ JS/MenuSlider.js
@@ -1,13 +1,13 @@
 let hamburger = document.getElementById("hamburger");
 let navBar = document.getElementById("nav-bar");
 
-function openMenu () {
+function openMenu () {                                  //slides menu bar from right to left. disables itself. enables hamburger to close menu bar, sliding left to right.
     navBar.style.left = "0px";
     hamburger.removeEventListener("click", openMenu);
     hamburger.addEventListener("click", closeMenu);
 }
 
-function closeMenu () {
+function closeMenu () {                                 //closes menu bar from left to right. disables itself, enables hamburger to open menu bar again.
     navBar.style.left = "100%";
     hamburger.removeEventListener("click", closeMenu);
     hamburger.addEventListener("click", openMenu);
